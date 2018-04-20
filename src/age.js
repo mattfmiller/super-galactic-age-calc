7c251add3b3f755c13e085f3e750c2260ead1e6e
Matt Miller <matt.f.miller@live.com>
2018-04-20 11:30:22 -0700

    Pass third test

--- src/age.js
+++ src/age.js
@@ -2,6 +2,7 @@ export class Age {
   constructor (birthDate) {
     this.birthDate = new Date(birthDate);
     this.earthYears = Math.floor((new Date() - this.birthDate)/31536000000);
+    this.seconds = Math.floor((new Date() - this.birthDate)/1000);
   }
 
   yearsToSeconds() {
@@ -13,4 +14,7 @@ export class Age {
     return (currentDate - this.birthDate)/1000;
   }
 
+  toMercuryYears() {
+    return Math.round((this.seconds/7568640)*100)/100; //seconds in a mercury year//
+  }
 }
