e367238a8b2d08961fcbb4c8d8aefae4c8105e7f
Matt Miller <matt.f.miller@live.com>
2018-04-20 09:39:04 -0700

    delete scripts.js and comment out front end logic.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-
-$(document).ready(function(){
-  
-});
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
+//
+// $(document).ready(function(){
+//
+// });
