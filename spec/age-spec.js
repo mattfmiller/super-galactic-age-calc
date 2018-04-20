ea619ab41a6594d0f8ffa8679993a5d185507bbf
Matt Miller <matt.f.miller@live.com>
2018-04-20 10:52:59 -0700

    Create and fail second test

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -2,14 +2,24 @@ import {Age} from './../src/age.js'
 
 describe('Test', function(){
   var age;
+  var birthDate;
+  var currentDate;
 
   beforeEach(function(){
-    age = new Age(1);
+    currentDate = "April 20, 2018";
+    birthDate = 'April 20, 2017';
+    age = new Age(birthDate);
+    console.log(age);
   });
 
   it('should correctly determine how many seconds are in a given number of Earth years', function(){
-    let seconds = age.toSeconds();
+    let seconds = age.yearsToSeconds();
     expect(seconds).toEqual(31536000);
+  });
+
+  it('should correctly determine the number of seconds that have elapsed between two given dates', function(){
+    let secondsBetweenDates = age.dateToSeconds(currentDate);
+    expect(secondsBetweenDates).toEqual(31536000);
   });
 
 });
